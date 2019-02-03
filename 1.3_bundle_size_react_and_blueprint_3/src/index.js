11d19f2758482d27c599aa4116dcf1189239a8fc
avevlad <vlad.open.source@gmail.com>
2019-02-03 20:31:22 +0300

    Add circle ci and blueprint 3.0

---
+++ 1.3_bundle_size_react_and_blueprint_3/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Button } from '@blueprintjs/core';
+
+
+const container = document.getElementById('app-container');
+
+ReactDOM.render((<div><Button>123</Button></div>), container);
+
+
